d73a6e7353fa82efe5e0085f8582fc9a05f4de76
Artem Yemelyanov <ae1666528@gmail.com>
2021-08-10 16:48:36 +0300

    Updates

---
+++ precache-manifest.dc526b1d5004502e0c5825dc57ee50d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87422ed8043917b33e107b79f2eb0789",
+    "url": "/auth_login/index.html"
+  },
+  {
+    "revision": "69ad69b69441aa733361",
+    "url": "/auth_login/static/css/2.1890a892.chunk.css"
+  },
+  {
+    "revision": "aec3f5d0f657ae5f9d5d",
+    "url": "/auth_login/static/css/main.3fb381b6.chunk.css"
+  },
+  {
+    "revision": "69ad69b69441aa733361",
+    "url": "/auth_login/static/js/2.8ee43561.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/auth_login/static/js/2.8ee43561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aec3f5d0f657ae5f9d5d",
+    "url": "/auth_login/static/js/main.a5486cca.chunk.js"
+  },
+  {
+    "revision": "24cd5ffd9fe20cb840ab",
+    "url": "/auth_login/static/js/runtime-main.9a43784c.js"
+  }
+]);
